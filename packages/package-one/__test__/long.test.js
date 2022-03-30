264cb64032f3118f43149b638e2e4fe41a863a93
Sean McCollum <sean.mccollum@veeva.com>
2022-03-30 16:34:36 -0700

    remove jest

--- packages/package-one/__test__/long.test.js
+++
@@ -1,5 +0,0 @@
-const delay = ms => new Promise((resolve) => setTimeout(resolve, ms))
-test('long test', async () => {
-    await delay(1000);
-    expect(true).toBe(true);
-})
--- packages/package-two/__test__/short.test.js
+++
@@ -1,3 +0,0 @@
-test('short failing test', () => {
-    expect(true).toBe(true);
-})
